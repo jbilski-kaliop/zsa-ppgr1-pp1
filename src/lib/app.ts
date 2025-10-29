5479296f0053d5b16b1fab6f19618da8ea36d431
Jakub Bilski <jakub@blsk.pl>
2025-10-29 10:34:15 +0100

    Moved runApp method to OOP context.

--- src/lib/app.ts
+++ src/lib/app.ts
@@ -1,49 +1,60 @@
-import { getIntValueFromInputById, clearInput } from './dom/input';
-import { gcd, isPrime } from './math';
+import { getIntValueFromInputById, clearInput } from "./dom/input";
+import { gcd, isPrime } from "./math";
 
-const INPUT_EL_1 = 'val1';
-const INPUT_EL_2 = 'val2';
-const ACTION_GCD_EL = 'calculateGcd';
-const ACTION_IS_PRIME_1_EL = 'checkIfPrime1';
-const ACTION_IS_PRIME_2_EL = 'checkIfPrime2';
+export default class App {
+  static readonly INPUT_EL_1 = "val1";
+  static readonly INPUT_EL_2 = "val2";
+  static readonly ACTION_GCD_EL = "calculateGcd";
+  static readonly ACTION_IS_PRIME_1_EL = "checkIfPrime1";
+  static readonly ACTION_IS_PRIME_2_EL = "checkIfPrime2";
 
-export default function(): void {
-    const calculateGcd = document.getElementById(ACTION_GCD_EL) as HTMLButtonElement;
-    calculateGcd?.addEventListener('click', function () {
-        const val1 = getIntValueFromInputById(INPUT_EL_1);
-        const val2 = getIntValueFromInputById(INPUT_EL_2);
+  public run() {
+    this.handleGcdAction();
+    this.handleIsPrimeActionOnValue(App.ACTION_IS_PRIME_1_EL, App.INPUT_EL_1);
+    this.handleIsPrimeActionOnValue(App.ACTION_IS_PRIME_2_EL, App.INPUT_EL_2);
+  }
 
-        if (isNaN(val1) || isNaN(val2)) {
-            console.error(`Check provided values ([${val1}], [${val2}]), at least one is NaN`);
-            return;
-        }
+  private handleGcdAction() {
+    const calculateGcd = document.getElementById(
+      App.ACTION_GCD_EL
+    ) as HTMLButtonElement;
+    calculateGcd?.addEventListener("click", function () {
+      const val1 = getIntValueFromInputById(App.INPUT_EL_1);
+      const val2 = getIntValueFromInputById(App.INPUT_EL_2);
 
-        console.log(`Value 1 = ${val1}, Value 2 = ${val2}`);
+      if (isNaN(val1) || isNaN(val2)) {
+        console.error(
+          `Check provided values ([${val1}], [${val2}]), at least one is NaN`
+        );
+        return;
+      }
 
-        const gcdValue = gcd(val1, val2);
+      console.log(`Value 1 = ${val1}, Value 2 = ${val2}`);
 
-        console.log(`We've found GCD for values [${val1}, ${val2}]: ${gcdValue}!`);
-        
-        clearInput(INPUT_EL_1, INPUT_EL_2);
+      const gcdValue = gcd(val1, val2);
+
+      console.log(
+        `We've found GCD for values [${val1}, ${val2}]: ${gcdValue}!`
+      );
+
+      clearInput(App.INPUT_EL_1, App.INPUT_EL_2);
     });
+  }
 
-    _handleIsPrimeActionOnValue(ACTION_IS_PRIME_1_EL, INPUT_EL_1);
-    _handleIsPrimeActionOnValue(ACTION_IS_PRIME_2_EL, INPUT_EL_2);
-}
-
-function _handleIsPrimeActionOnValue(actionElId: string, inputElId: string) {
+  private handleIsPrimeActionOnValue(actionElId: string, inputElId: string) {
     const actionEl = document.getElementById(actionElId) as HTMLButtonElement;
-    actionEl?.addEventListener('click', () => {
-        const value = getIntValueFromInputById(inputElId);
-        if (isNaN(value)) {
-            console.error(`Provided value "${value}" is invalid.`);
-            return;
-        }
+    actionEl?.addEventListener("click", () => {
+      const value = getIntValueFromInputById(inputElId);
+      if (isNaN(value)) {
+        console.error(`Provided value "${value}" is invalid.`);
+        return;
+      }
 
-        const result = isPrime(value);
+      const result = isPrime(value);
 
-        console.log(`Value "${value}" is${result ? '' : ' not'} prime`);
+      console.log(`Value "${value}" is${result ? "" : " not"} prime`);
 
-        clearInput(inputElId);
+      clearInput(inputElId);
     });
+  }
 }
